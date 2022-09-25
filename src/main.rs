1a6d5df8c6a28ec5e494cdbd8c222d415f12f43f
Tim Janke <tim+github@janke.biz>
2022-09-25 13:21:46 +0200

    :sparkles: Add support for SHA256

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,12 @@ enum Algorithms {
         #[arg(value_parser)]
         input_text: String,
     },
+    /// Use the SHA256 algorithm for hashing.
+    SHA256 {
+        /// The text which should be hashed.
+        #[arg(value_parser)]
+        input_text: String,
+    },
 }
 
 fn hash_bcrypt(cost: &u32, text: &String) -> String {
@@ -28,6 +34,11 @@ fn hash_bcrypt(cost: &u32, text: &String) -> String {
     };
 }
 
+fn hash_sha256(text: &String) -> String {
+    use sha2::{Digest, Sha256};
+    format!("{:x}", Sha256::digest(text))
+}
+
 fn main() {
     // parse the supplied arguments
     let arguments = Args::parse();
@@ -35,5 +46,6 @@ fn main() {
     // based on the supplied algorithm, calculate and output the hash
     match &arguments.algorithm {
         Algorithms::Bcrypt { cost, input_text } => println!("{}", hash_bcrypt(cost, input_text)),
+        Algorithms::SHA256 {input_text} => println!("{}", hash_sha256(input_text)),
     }
 }
