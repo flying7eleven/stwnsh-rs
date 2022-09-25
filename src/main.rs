b177eb396377f17d934bb2f9632c267240b15acb
Tim Janke <tim+github@janke.biz>
2022-09-25 12:24:45 +0200

    :sparkles: Add support for bcrypt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The algorithm which should be used for hashing.
+    #[clap(subcommand)]
+    algorithm: Option<Algorithms>,
+}
+
+#[derive(Subcommand)]
+enum Algorithms {
+    /// Use the BCrypt algorithm for hashing.
+    Bcrypt {
+        /// The cost value for hashing the input text.
+        #[arg(short, long, default_value_t = 12)]
+        cost: u32,
+        /// The text which should be hashed.
+        #[arg(value_parser)]
+        input_text: String,
+    },
+}
+
+fn hash_bcrypt(cost: &u32, text: &String) -> String {
+    return match bcrypt::hash(text, cost.clone()) {
+        Ok(hashed_text) => hashed_text,
+        Err(error) => format!("ERROR: {}", error),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse the supplied arguments
+    let arguments = Args::parse();
+
+    // based on the supplied algorithm, calculate and output the hash
+    match &arguments.algorithm {
+        Some(Algorithms::Bcrypt { cost, input_text }) => println!("{}", hash_bcrypt(cost, input_text)),
+        None => {}
+    }
 }
